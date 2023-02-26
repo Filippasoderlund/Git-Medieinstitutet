3f2e66eda4b86962d48bd48201be906d89b71867
Filippasoderlund <filippa.soderlund@medieinstitutet.se>
2023-02-26 13:21:39 +0100

    html remove

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,6 @@
       Har idetorka för tillfället så detta duger för nu. 
     </article>
     <div>
-      <span>Detta är endast för att ha mer på sidan</span>
-      <span>Även mer extra text</span>
-    </div>
-    <h1>Hobby</h1>
-    <button>Test</button>
-    <article>
-      Detta är en uppgift för att lära sig mer i git, har även valt att testa på vue och ts. <br>
-      Har idetorka för tillfället så detta duger för nu. 
-    </article>
-    <div>
       <span>Detta är endast för att ha mer på sidan</span><br>
       <span>Även mer extra text</span>
       <span>test span</span>
